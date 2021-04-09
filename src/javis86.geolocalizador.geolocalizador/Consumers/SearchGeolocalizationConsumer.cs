4f45cd29518aead45cbb337edaab109c3a7511af
Javier Colombera <jcolombera@gmail.com>
2021-04-09 18:00:50 -0300

    Integración nominatim api

--- src/javis86.geolocalizador.geolocalizador/Consumers/SearchGeolocalizationConsumer.cs
+++ src/javis86.geolocalizador.geolocalizador/Consumers/SearchGeolocalizationConsumer.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using javis86.Contracts;
 using MassTransit;
 using Microsoft.Extensions.Logging;
+using Nominatim.API.Geocoders;
+using Nominatim.API.Models;
 
 namespace javis86.geolocalizador.geolocalizador.Consumers
 {
@@ -17,7 +19,22 @@ namespace javis86.geolocalizador.geolocalizador.Consumers
 
         public async Task Consume(ConsumeContext<ISearchGeolocalization> context)
         {
-            _logger.LogDebug($"Llegó la petición {context.Message.Calle}");
+            var geocoder = new ForwardGeocoder();
+
+            var r = geocoder.Geocode(new ForwardGeocodeRequest {
+                queryString = $"{context.Message.Calle} {context.Message.Numero} {context.Message.Ciudad}, {context.Message.Provincia}, {context.Message.Pais}",
+
+                BreakdownAddressElements = true,
+                ShowExtraTags = false,
+                ShowAlternativeNames = true,
+                ShowGeoJSON = true
+            });
+            
+            r.Wait();
+
+            if (r.Result.Length > 0)
+                _logger.LogDebug($"{r.Result[0].Latitude} {r.Result[0].Longitude}");
+
             await Task.CompletedTask;
         }
     }
